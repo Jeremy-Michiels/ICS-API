7047c5e9510707013a2f6996de65f1a557202908
Jeremy-Michiels <64803899+Jeremy-Michiels@users.noreply.github.com>
2024-12-06 14:07:54 +0100

    fw

--- Models/misc/CONF.cs
+++
@@ -1,4 +0,0 @@
-public class Conf{
-    public static readonly string tenantId = "https://login.microsoftonline.com/0cedde87-baed-4803-acec-0c1144a4b69c";
-    public static readonly string clientId = "9abc6473-ac5b-408a-98d3-869d3e6fb3d4";
-}
